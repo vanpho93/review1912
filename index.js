be666fd7e82aa2ef63473b0e01a6044c640bd3ea
Admin <vanpho02@gmail.com>
2017-01-11 21:17:14 +0700

    xong

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ app.listen(3000, ()=> console.log('Server started'));
 
 class SanPham{
   constructor(title, desc, image, video){
-
+    this.title = title;
+    this.desc = desc;
+    this.image = image;
+    this.video = video;
   }
 }
-app.get('/', (req, res) => res.render('home'));
+
+var sp1 = new SanPham('Yearbook', `Such a wonderful experience to watch this without knowing anything about it, enjoy it, and then slowly realise it's based on my fabvourite graphic novel.`,'1.png',84327247);
+var sp2 = new SanPham('Christmas', `this is the best christmas video i have ever seen. fricken fantastic!`, '2.png', 196277011);
+var sp3 = new SanPham('FLUSHED', `Ah this is awesome thanks so much for your kind words & featuring us, going through your previous weekend watching lists now! :)`, '3.png', 197047485);
+var mangSanPham = [sp1, sp2, sp3];
+app.get('/', (req, res) => res.render('home', {mangSanPham}));
