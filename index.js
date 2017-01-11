61c11ba2e4d2615d440bec207960345ba9c1c6f1
Admin <vanpho02@gmail.com>
2017-01-11 20:59:23 +0700

    xong

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public'));
+app.listen(3000, ()=> console.log('Server started'));
+
+class SanPham{
+  constructor(title, desc, image, video){
+
+  }
+}
+app.get('/', (req, res) => res.render('home'));
